559333e5c3a4e83fd3a6a593d3c46e6f646f9157
tomykaira <tomykaira@gmail.com>
2013-10-14 19:43:41 +0900

    Forcefully load built shared object from recognizer

--- selector/src/main/groovy/io/github/tomykaira/accelvoice/selector/RecognizerLibrary.groovy
+++ selector/src/main/groovy/io/github/tomykaira/accelvoice/selector/RecognizerLibrary.groovy
@@ -2,11 +2,23 @@ package io.github.tomykaira.accelvoice.selector
 
 import com.sun.jna.Library
 import com.sun.jna.Native
+import com.sun.jna.NativeLibrary
 
 public interface RecognizerLibrary extends Library {
-    RecognizerLibrary INSTANCE = (RecognizerLibrary) Native.loadLibrary("recognizer", RecognizerLibrary.class);
+    RecognizerLibrary INSTANCE = RecognizerLibrary.LoadHelper.loadLibrary()
 
     void start(int argc, String []argv);
     void stop();
-    String recognize(String[] candidates);
+    int recognize(String[][] candidates, String[] unknown);
+
+    private class LoadHelper {
+        static RecognizerLibrary loadLibrary() {
+            def path = System.getProperty("java.class.path")
+                    .split(":")
+                    .find { it.contains("libaccel_recognizer.so") }
+                    .replace("/libaccel_recognizer.so", "")
+            NativeLibrary.addSearchPath("accel_recognizer", path)
+            Native.loadLibrary("accel_recognizer", RecognizerLibrary.class) as RecognizerLibrary;
+        }
+    }
 }
