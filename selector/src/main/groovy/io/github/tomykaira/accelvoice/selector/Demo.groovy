89aeeede1e464b7ae6ac5bc8b61b65165aaa31d1
tomykaira <tomykaira@gmail.com>
2013-10-25 11:17:49 +0900

    Fix broken demo code

--- selector/src/main/groovy/io/github/tomykaira/accelvoice/selector/Demo.groovy
+++ selector/src/main/groovy/io/github/tomykaira/accelvoice/selector/Demo.groovy
@@ -2,16 +2,28 @@ package io.github.tomykaira.accelvoice.selector
 
 class Demo {
     public static void main(String[] args) {
+        def done = false
+        def listener = new SelectionListener() {
+            @Override
+            void notify(String selected) {
+                println(selected)
+                done = true
+            }
+        }
         def collection = new CandidatesCollection(["addAll", "asBoolean", "asImmutable", "asList", "asSynchronized",
                 "asType", "collect", "collectAll", "collectEntries", "collectMany", "collectNested", "combinations",
                 "containsAll", "count", "countBy", "disjoint", "eachPermutation", "find", "findAll", "findResult",
                 "findResults", "flatten", "getAt", "grep", "groupBy", "inject", "intersect", "isCase", "join",
                 "leftShift", "max", "min", "multiply", "plus", "removeAll", "retainAll", "sort", "split", "sum",
-                "toList", "toListString", "toSet", "unique"])
+                "toList", "toListString", "toSet", "unique"], listener)
 
-        RecognizerLibrary.INSTANCE.start(args.length, args)
+        RecognizerLibrary.INSTANCE.start(args.length, args, null)
 
-        println(collection.select())
+        collection.select()
+
+        while (!done) {
+            sleep(1000)
+        }
 
         RecognizerLibrary.INSTANCE.stop()
     }
